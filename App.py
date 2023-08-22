633ef70abba7e136d8a59b57985bc135f09e2266
USH-TECHNOLOGY <104039305+USH-TECHNOLOGY@users.noreply.github.com>
2023-08-22 13:29:38 -0400

    Create App.py

---
+++ App.py
@@ -0,0 +1,53 @@
+from dotenv import load_dotenv
+import os
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title ="Ask your PDF")
+    st.header("Ask your PDF")
+
+    #Uploading File
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+
+#Extracting Text
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text+= page.extrat_text()
+
+        #split into chunks
+        text_splitter = CharacterTextSplitter(
+            seperator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+            )
+
+        chunks = text_splitter.split_text(text)
+
+#Create Embeddings
+        embeddings = OpenAIEmbeddings
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+    
+#Show User Input
+    user_question = st.text_input("Ask a question about your pdf")
+    if user_question:
+        docs = knowledge_base.similarity_search(user_question)
+
+        llm = OpenAI()
+        chain = load_qa_chain(llm, chain_type="stuff")
+        response = chain.run(input_documents=docs,question=user_question)
+
+        st.write(response)
+
+if __name__ == '__main__':
+    main()
